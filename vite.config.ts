091b7aaebde23b4d122ff85ffb743ae5da6b2b82
kayac-chang <egg734631@gmail.com>
2021-10-19 21:02:07 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactSvgPlugin from "vite-plugin-react-svg";
+import path from "path";
+
+export default defineConfig({
+  plugins: [reactRefresh(), reactSvgPlugin({ defaultExport: "component" })],
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
+});
